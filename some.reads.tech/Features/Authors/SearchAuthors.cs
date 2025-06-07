e02f17e5cbc0cf7e8a6d14c5dca152ba811b48f6
Martin Cardenas <martiinca.dev@gmail.com>
2025-06-07 01:38:52 -0700

    feature: implement CacheEndpointFilter for Search Authors feature

--- some.reads.tech/Features/Authors/SearchAuthors.cs
+++ some.reads.tech/Features/Authors/SearchAuthors.cs
@@ -1,6 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Caching.Memory;
-using some.reads.tech.Common;
+﻿using some.reads.tech.Common;
+using some.reads.tech.Filters;
 using some.reads.tech.Services;
 
 namespace some.reads.tech.Features.Authors
@@ -9,23 +8,13 @@ namespace some.reads.tech.Features.Authors
     {
         public static void AddSearchAuthorsEndpoints(this IEndpointRouteBuilder app)
         {
-            app.MapGet("authors/search", Handler);
+            app.MapGet("authors/search", Handler).AddEndpointFilter<CacheEndpointFilter>();
         }
 
-        private static async Task<IResult> Handler(
-            [FromQuery] string name,
-            [FromServices] OpenLibraryService openLibraryService,
-            [FromServices] IMemoryCache memoryCache
-            )
-        {
-            string cacheKey = $"authors_search_{name}";
-
-            var response = await memoryCache.GetOrCreateAsync(cacheKey, async entry =>
-            {
-                entry.SetAbsoluteExpiration(TimeSpan.FromMinutes(5));
-                return await openLibraryService.GetFromJsonAsync<OpenLibraryResponse<AuthorResponse>?>($"search/authors.json?q={name}");
-            });
-
+        private static async Task<IResult> Handler(string name, OpenLibraryService openLibraryService)
+        { 
+            var response = await openLibraryService.GetFromJsonAsync<OpenLibraryResponse<AuthorResponse>?>($"search/authors.json?q={name}");
+            
             if (response is null || response.NumFound == 0) return Results.NotFound(new { message = "No authors found" });
 
             var authors = response.Docs
